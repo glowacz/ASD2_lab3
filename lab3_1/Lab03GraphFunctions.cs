298aca3b7da5b88b8b545a34973338f17d586263
glowacz <glowacki.pj@gmail.com>
2023-03-09 11:47:40 +0100

    Add project files.

---
+++ lab3_1/Lab03GraphFunctions.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using ASD.Graphs;
+using ASD;
+using System.Collections.Generic;
+
+namespace ASD
+{
+
+    public class Lab03GraphFunctions : System.MarshalByRefObject
+    {
+
+        // Część 1
+        // Wyznaczanie odwrotności grafu
+        //   0.5 pkt
+        // Odwrotność grafu to graf skierowany o wszystkich krawędziach przeciwnie skierowanych niż w grafie pierwotnym
+        // Parametry:
+        //   g - graf wejściowy
+        // Wynik:
+        //   odwrotność grafu
+        // Uwagi:
+        //   1) Graf wejściowy pozostaje niezmieniony
+        //   2) Graf wynikowy musi być w takiej samej reprezentacji jak wejściowy
+        public DiGraph Lab03Reverse(DiGraph g)
+        {
+            DiGraph newg = new DiGraph(g.VertexCount, g.Representation);
+            for(int v=0; v<g.VertexCount; v++) 
+            { 
+                foreach(int u in g.OutNeighbors(v))
+                    newg.AddEdge(u, v);
+            }
+            return newg;
+        }
+
+        // Część 2
+        // Badanie czy graf jest dwudzielny
+        //   0.5 pkt
+        // Graf dwudzielny to graf nieskierowany, którego wierzchołki można podzielić na dwa rozłączne zbiory
+        // takie, że dla każdej krawędzi jej końce należą do róźnych zbiorów
+        // Parametry:
+        //   g - badany graf
+        //   vert - tablica opisująca podział zbioru wierzchołków na podzbiory w następujący sposób
+        //          vert[i] == 1 oznacza, że wierzchołek i należy do pierwszego podzbioru
+        //          vert[i] == 2 oznacza, że wierzchołek i należy do drugiego podzbioru
+        // Wynik:
+        //   true jeśli graf jest dwudzielny, false jeśli graf nie jest dwudzielny (w tym przypadku parametr vert ma mieć wartość null)
+        // Uwagi:
+        //   1) Graf wejściowy pozostaje niezmieniony
+        //   2) Podział wierzchołków może nie być jednoznaczny - znaleźć dowolny
+        //   3) Pamiętać, że każdy z wierzchołków musi być przyporządkowany do któregoś ze zbiorów
+        //   4) Metoda ma mieć taki sam rząd złożoności jak zwykłe przeszukiwanie (za większą będą kary!)
+        public bool Lab03IsBipartite(Graph g, out int[] vert)
+        {
+            vert = new int[g.VertexCount];
+
+            foreach (Edge e in g.DFS().SearchAll())
+            {
+                if (vert[e.From] == 0 || vert[e.From] == 1)
+                {
+                    vert[e.From] = 1;
+                    if (vert[e.To] == 1)
+                    {
+                        vert = null;
+                        return false;
+                    }
+                    vert[e.To] = 2;
+                }
+                else
+                {
+                    //vert[e.From] = 2;
+                    if (vert[e.To] == 2)
+                    {
+                        vert = null;
+                        return false;
+                    }
+                    vert[e.To] = 1;
+                }
+            }
+
+            for(int v=0; v<g.VertexCount; v++)
+            {
+                if (vert[v] == 0)
+                    vert[v] = 1;
+            }
+
+            return true;
+        }
+
+        // Część 3
+        // Wyznaczanie minimalnego drzewa rozpinającego algorytmem Kruskala
+        //   1 pkt
+        // Schemat algorytmu Kruskala
+        //   1) wrzucić wszystkie krawędzie do "wspólnego worka"
+        //   2) wyciągać z "worka" krawędzie w kolejności wzrastających wag
+        //      - jeśli krawędź można dodać do drzewa to dodawać, jeśli nie można to ignorować
+        //      - punkt 2 powtarzać aż do skonstruowania drzewa (lub wyczerpania krawędzi)
+        // Parametry:
+        //   g - graf wejściowy
+        //   mstw - waga skonstruowanego drzewa (lasu)
+        // Wynik:
+        //   skonstruowane minimalne drzewo rozpinające (albo las)
+        // Uwagi:
+        //   1) Graf wejściowy pozostaje niezmieniony
+        //   2) Wykorzystać klasę UnionFind z biblioteki Graph
+        //   3) Jeśli graf g jest niespójny to metoda wyznacza las rozpinający
+        //   4) Graf wynikowy (drzewo) musi być w takiej samej reprezentacji jak wejściowy
+        public Graph<int> Lab03Kruskal(Graph<int> g, out int mstw)
+        {
+            PriorityQueue<int, Edge<int> > q = new PriorityQueue< int, Edge<int> >();
+            mstw = 0;
+
+            for (int v = 0; v < g.VertexCount; v++)
+            {
+                foreach (Edge<int> e in g.OutEdges(v))
+                {
+                    if(v < e.To)
+                        q.Insert(e, e.Weight);
+                }        
+            }
+
+            Graph<int> tree = new Graph<int>(g.VertexCount, g.Representation);
+            UnionFind uf = new UnionFind(g.VertexCount);
+
+            while (q.Count > 0)
+            {
+                Edge<int> e = q.Extract();
+                if (uf.Find(e.From) == uf.Find(e.To))
+                    continue;
+                uf.Union(e.From, e.To);
+                tree.AddEdge(e.From, e.To);
+                mstw += e.Weight;
+            }
+
+            
+            return tree;
+        }
+
+        // Część 4
+        // Badanie czy graf nieskierowany jest acykliczny
+        //   0.5 pkt
+        // Parametry:
+        //   g - badany graf
+        // Wynik:
+        //   true jeśli graf jest acykliczny, false jeśli graf nie jest acykliczny
+        // Uwagi:
+        //   1) Graf wejściowy pozostaje niezmieniony
+        //   2) Najpierw pomysleć jaki, prosty do sprawdzenia, warunek spełnia acykliczny graf nieskierowany
+        //      Zakodowanie tego sprawdzenia nie powinno zająć więcej niż kilka linii!
+        //      Zadanie jest bardzo łatwe (jeśli wydaje się trudne - poszukać prostszego sposobu, a nie walczyć z trudnym!)
+        public bool Lab03IsUndirectedAcyclic(Graph g)
+        {
+            return false;
+        }
+
+    }
+
+}
